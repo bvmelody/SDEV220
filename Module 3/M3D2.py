ed7989444b1f8612af7c5ee31dd2fd58448bd7c8
bvmelody <131800286+bvmelody@users.noreply.github.com>
2026-01-28 17:56:01 -0800

    CLass edits

---
+++ Module 3/M3D2.py
@@ -0,0 +1,11 @@
+lst = (1,2,3,4,5,6,7)
+k = 2
+
+for x in range(len(lst)):
+    if k == lst[x]:
+        is_found = True
+        print(f"{k} has an index of {x}")
+        break
+    
+if is_found == False:
+    print(f"{k} was not found in the list")
